0f8e6372f18577d3973500aa565788c4ef8416f9
Neil Mwaki <muthamineil@gmail.com>
2024-05-28 10:43:36 +0300

    program to replace one or more blanks by a single blanks in each string

---
+++ exercise/exe1-9.c
@@ -0,0 +1,15 @@
+/* copy input to its output, replacing
+    each string of one or more blanks by a single blank */
+#include <stdio.h>
+
+int main(){
+    int c;
+
+    while(((c = getchar()) != EOF)){
+        if(c == ' '){
+            while ((c = getchar()) == ' ');
+            putchar(' ');
+        }
+        putchar(c);
+    }
+}
